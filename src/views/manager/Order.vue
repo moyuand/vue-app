0f296ad40adf56b1e3eeac37f05859501c6bf96c
moyuand <sxaufujie@163.com>
2020-01-07 23:00:48 +0800

    first02

--- src/views/manager/Order.vue
+++ src/views/manager/Order.vue
@@ -1,26 +1,13 @@
 <template>
   <div class="order">
     <van-nav-bar title="我的订单" />
-    <van-tabs v-model="active" color="#1659a0" @click="tabClickHandler(name,title)">
-      <van-tab title="全部">
-        <div>
-      <!-- <briup-order-item 
-        v-for="o in orders" 
-        :key="o.id" 
-        :data="o">
-      </briup-order-item> -->
-      </div> 
-      </van-tab>
-      <van-tab title="待派单">
-      </van-tab>
-      <van-tab title="待接单">
-      </van-tab>
-      <van-tab title="待服务">
-      </van-tab> 
-      <van-tab title="待确认">
-      </van-tab>
-      <van-tab title="已完成">
-      </van-tab>
+    <van-tabs @click="tabClickHandler" v-model="active" color="#1659a0">
+      <van-tab title="全部"></van-tab>
+      <van-tab title="待派单"></van-tab>
+      <van-tab title="待接单"></van-tab> 
+      <van-tab title="待服务"></van-tab> 
+      <van-tab title="待确认"></van-tab> 
+      <van-tab title="已完成"></van-tab>
     </van-tabs>
     <!-- 订单 -->
     <div>
@@ -41,7 +28,8 @@ export default {
   data(){
     return {
       active:0,
-      orders:[]
+      orders:[],
+      status:null
     }
   },
   computed:{
@@ -51,6 +39,12 @@ export default {
     this.loadOrders();
   },
   methods:{
+    // 点击tab的时候执行的回调函数
+    tabClickHandler(name,title){
+      // 修改当前data中status的值； 重新加载订单
+      this.status = title === "全部" ? null : title;
+      this.loadOrders();
+    },
     // 加载我的订单
     loadOrders(){
       let url = "/order/query";
@@ -61,11 +55,6 @@ export default {
       get(url,params).then((response)=>{
         this.orders = response.data;
       })
-    },
-    tabClickHandler(name,title){
-      this.status=title==="全部"?null:title;
-      this.loadOrders();
-
     }
   }
 }
